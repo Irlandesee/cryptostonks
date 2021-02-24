c131ad69b43d82105d81aadbeb44138524c4949d
Mattia Lunardi <mattia.lunardi@gmail.com>
2021-02-24 10:18:11 +0100

    Second Commit
    Created Main.java and Crypto.java
    Main reads from data and builds
    hashmaps<localDataTime, crypto>
    Moved historicaldata into appropriate directories

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,106 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
+import java.util.HashMap;
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import crypto.Crypto;
 
 public class Main{
 
-	
+	private static String buildPath(String fileName){
+		if(fileName.toLowerCase().equals("binancecoin_eur.txt"))
+			return "../historicaldata/binancecoin_eur/binanceCoin_eur.txt";
+		else if(fileName.toLowerCase().equals("bitcoin_eur.txt"))
+			return "../historicaldata/bitcoin_eur/bitcoin_eur.txt";
+		else if(fileName.toLowerCase().equals("ethereum_eur.txt"))
+			return "../historicaldata/ethereum_eur/ethereum_eur.txt";
+		else if(fileName.toLowerCase().equals("polkadot_eur.txt"))
+			return "../historicaldata/ethereum_eur/polkadot_eur.txt";
+		return "Could not build pathname";
+	}
+
+	private static String[] splitString(String s){
+		return s.split(",");
+	}
+
+	private static LocalDateTime parseTime(String time){
+		String[] timeSplitted = time.split(" ");
+		String ris = timeSplitted[0]+"T"+timeSplitted[1];
+		return LocalDateTime.parse(ris);
+	}
+
+	private static HashMap<LocalDateTime, Crypto> readData(File f){
+		System.out.println("Started reading from file: "+f.getName());
+		HashMap<LocalDateTime, Crypto> ris = new HashMap<LocalDateTime, Crypto>();
+		Crypto c;
+		try{
+			BufferedReader buf = new BufferedReader(new FileReader(f));
+			String stringaLetta = "";
+			String[] stringaSplittata = new String[7]; //lunghezza di una riga splittata
+			while((stringaLetta = buf.readLine()) != null){
+				stringaSplittata = splitString(stringaLetta);
+				String cryptoName = stringaSplittata[0];
+				String timeString = stringaSplittata[1];
+				LocalDateTime time = parseTime(timeString);
+
+				c = new Crypto(cryptoName, time);
+				c.setParameters(stringaSplittata[2], stringaSplittata[3], stringaSplittata[4],
+						stringaSplittata[5], stringaSplittata[6]);
+
+				ris.put(time, c);
+			}
+
+			buf.close();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+
+		return ris;
+	}
+
 
 	public static void main(String[] args){
+		/**
+		LocalDateTime now = LocalDateTime.now();
+		System.out.println("Now :"+now);
+		String cryptoName = "BTC-EUR";
+
+
+		Crypto c = new Crypto(cryptoName, now);
+		//System.out.println(c.toString());
+
+		HashMap<LocalDateTime, Crypto> hMap = new HashMap<LocalDateTime, Crypto>();
+		hMap.put(now, c);
+
+		//System.out.println(hMap.toString());
+		Crypto prova = hMap.get(now);
+		System.out.println("Printing prova: ");
+		System.out.println(prova.toString());
+		**/
+
+		/**
+		String time = "2021-01-25 23:00:00";
+
+		LocalDateTime prova = parseTime(time);
+
+		System.out.println("prova: "+prova);
+		**/
+
+		if(args.length < 1)
+			System.out.println("Inserisci input file");
+		else{
+			String fileName = args[0];
+			try{
+
+
+			}catch(NullPointerException e){
+				e.printStackTrace();
+			}
+		}
 
 	}
 
--- src/crypto/Crypto.java
+++ src/crypto/Crypto.java
@@ -12,8 +12,81 @@ public class Crypto{
 	private double open;
 	private double volume;
 
-	public Crypto(){
-		
+	public Crypto(String symbolName, LocalDateTime dateTime){
+		this.symbolName = symbolName;
+		this.dateTime = dateTime;
+	}
+
+	public String getSymbolName() {
+		return symbolName;
+	}
+
+	public void setSymbolName(String symbolName) {
+		this.symbolName = symbolName;
+	}
+
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(LocalDateTime dateTime) {
+		this.dateTime = dateTime;
+	}
+
+	public double getClose() {
+		return close;
+	}
+
+	public void setClose(double close) {
+		this.close = close;
+	}
+
+	public double getHigh() {
+		return high;
+	}
+
+	public void setHigh(double high) {
+		this.high = high;
+	}
+
+	public double getLow() {
+		return low;
+	}
+
+	public void setLow(double low) {
+		this.low = low;
+	}
+
+	public double getOpen() {
+		return open;
+	}
+
+	public void setOpen(double open) {
+		this.open = open;
+	}
+
+	public double getVolume() {
+		return volume;
+	}
+
+	public void setVolume(double volume) {
+		this.volume = volume;
+	}
+
+	public void setParameters(String close, String high, String low, String open, String volume){
+		this.setClose(Double.parseDouble(close));
+		this.setHigh(Double.parseDouble(high));
+		this.setLow(Double.parseDouble(low));
+		this.setOpen(Double.parseDouble(open));
+		this.setVolume(Double.parseDouble(volume));
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	public String toString(){
+		return this.symbolName + this.dateTime;
 	}
 
 }
